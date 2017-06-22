f6fbba96b04e97c8e2f041a1d35d00733d9e32d7
sungs <sungs@DESKTOP-B3JV703>
2017-06-22 22:19:30 +0900

    First_Push

--- udemy_coding_interview_kor/src/stack/arrayListStack/Practice_01.java
+++ udemy_coding_interview_kor/src/stack/arrayListStack/Practice_01.java
@@ -3,11 +3,18 @@ package stack.arrayListStack;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Practice_01 {
-   // private List<T> list;
+public class Practice_01<T> {
+	private List<T> list;
     
     public Practice_01(){
-    //    list = new ArrayList();
+        list = new ArrayList<T>();
     }
     
+    public void push(T i){
+    	list.add(i);
+    }
+    
+    public T pop(){
+    	return list.get(list.size()-1);
+    }
 }
